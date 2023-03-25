c52d03dd4b580faeb75449aaa5fe84bc7b1dc24d
Mohammed Sufiyan <116337828+7R1XY@users.noreply.github.com>
2023-03-25 14:42:46 +0530

    Add files via upload
    AreaOfCircle

---
+++ areaOfCircle.java
@@ -0,0 +1,16 @@
+class main{
+	public static void main(String[] args)
+	{
+		int radius;
+		double pi = 3.142, area;
+		radius = 23;
+		
+		area = pi * radius * radius;
+		
+		System.out.println("Area of Circle :" + area);
+	}
+}
+
+
+
+
